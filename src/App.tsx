d9a43f1ce656a371acbf85489b5fb002d795d5ab
whatariver <kubilayserhat@hotmail.com>
2024-08-26 16:46:02 +0300

    Local data storage
    The data of energy and points are now stored locally

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import { useEffect, useState } from 'react';
 import './index.css';
 import Arrow from './icons/Arrow';
-import { bear, coin, highVoltage, logo, rocket, trophy } from './images';
+import { coin, highVoltage, logo, rocket, trophy } from './images';
 
 const App = () => {
-  const [points, setPoints] = useState(1);
-  const [energy, setEnergy] = useState(2532);
+  const [points, setPoints] = useState<number>(() => {
+    // Check local storage for saved points
+    const savedPoints = localStorage.getItem('points');
+    return savedPoints ? parseInt(savedPoints, 10) : 1; 
+  });
+  
+  const [energy, setEnergy] = useState<number>(() => {
+    // Check local storage for saved energy
+    const savedEnergy = localStorage.getItem('energy');
+    return savedEnergy ? parseInt(savedEnergy, 10) : 50;
+  });
+  
   const [clicks, setClicks] = useState<{ id: number, x: number, y: number }[]>([]);
   const pointsToAdd = 1;
   const energyToReduce = 1;
+  const maxEnergy = 100;
 
   const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     if (energy - energyToReduce < 0) {
       return;
     }
+
     const rect = e.currentTarget.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
@@ -30,22 +42,29 @@ const App = () => {
   // useEffect hook to restore energy over time
   useEffect(() => {
     const interval = setInterval(() => {
-      setEnergy((prevEnergy) => Math.min(prevEnergy + 1, 6500));
-    }, 100); // Restore 10 energy points every second
+      setEnergy((prevEnergy) => Math.min(prevEnergy + 1, maxEnergy));
+    }, 1000);
 
-    return () => clearInterval(interval); // Clear interval on component unmount
+    return () => clearInterval(interval);
   }, []);
+
+  // useEffect to save points and energy to localStorage whenever they change
+  useEffect(() => {
+    localStorage.setItem('points', points.toString());
+  }, [points]);
+
+  useEffect(() => {
+    localStorage.setItem('energy', energy.toString());
+  }, [energy]);
 
   return (
     <div className="bg-gradient-main min-h-screen px-4 flex flex-col items-center text-white font-medium">
-
       <div className="absolute inset-0 h-1/2 bg-gradient-overlay z-0"></div>
       <div className="absolute inset-0 flex items-center justify-center z-0">
         <div className="radial-gradient-overlay"></div>
       </div>
 
       <div className="w-full z-10 min-h-screen flex flex-col items-center text-white">
-
         <div className="fixed top-0 left-0 w-full px-4 pt-8 z-10 flex flex-col items-center text-white">
           <div className="w-full cursor-pointer">
             <div className="bg-[#1f1f1f] text-center py-2 rounded-xl">
@@ -62,25 +81,19 @@ const App = () => {
           </div>
         </div>
 
-
         <div className="fixed bottom-0 left-0 w-full px-4 pb-4 z-10">
           <div className="w-full flex justify-between gap-2">
-            <div className="w-1/3 flex items-center justify-start max-w-32">
+            <div className="w-1/2 flex items-center justify-start max-w-32">
               <div className="flex items-center justify-center">
                 <img src={highVoltage} width={44} height={44} alt="High Voltage" />
                 <div className="ml-2 text-left">
                   <span className="text-white text-2xl font-bold block">{energy}</span>
-                  <span className="text-white text-large opacity-75">/ 6500</span>
+                  <span className="text-white text-large opacity-75">/ {maxEnergy}</span>
                 </div>
               </div>
             </div>
             <div className="flex-grow flex items-center max-w-60 text-sm">
               <div className="w-full bg-[#fad258] py-4 rounded-2xl flex justify-around">
-                <button className="flex flex-col items-center gap-1">
-                  <img src={bear} width={24} height={24} alt="High Voltage" />
-                  <span>Frens</span>
-                </button>
-                <div className="h-[48px] w-[2px] bg-[#fddb6d]"></div>
                 <button className="flex flex-col items-center gap-1">
                   <img src={coin} width={24} height={24} alt="High Voltage" />
                   <span>Earn</span>
@@ -94,10 +107,9 @@ const App = () => {
             </div>
           </div>
           <div className="w-full bg-[#f9c035] rounded-full mt-4">
-            <div className="bg-gradient-to-r from-[#f3c45a] to-[#fffad0] h-4 rounded-full" style={{ width: `${(energy / 6500) * 100}%` }}></div>
+            <div className="bg-gradient-to-r from-[#f3c45a] to-[#fffad0] h-4 rounded-full" style={{ width: `${(energy / maxEnergy) * 100}%` }}></div>
           </div>
         </div>
-
 
         <div className="flex-grow flex items-center justify-center">
           <div className="relative mt-4" onClick={handleClick}>
@@ -118,7 +130,6 @@ const App = () => {
             ))}
           </div>
         </div>
-
       </div>
     </div>
   );
